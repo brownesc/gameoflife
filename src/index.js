ff93fabb480b711a6ec4fd21b29aa64feab4ac7a
brownesc <andresuncle@gmail.com>
2019-08-14 02:07:30 -0400

    Upate With Background

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Grid extends React.Component {
+    render (){
+        return (
+            <div>Grid</div>
+        );
+    }
+}
+
+class Main extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            generation: 0,
+        }
+    }
+    render () {
+        return (
+            <div>
+                <h1> The Game of Life </h1>
+                <Grid />
+                <h2> Genrations: {this.state.generation} </h2>    
+            </div>
+
+        );
+    }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
